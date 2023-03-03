793bb4c0c538c0113e8eed681cc61977ddfeb10d
mark <mark.rizkalla1@gmail.com>
2023-03-03 18:48:42 +0200

    codeToHideStatusBar

--- app/src/main/java/com/markrizkalla/dishapp/SplashActivity.kt
+++ app/src/main/java/com/markrizkalla/dishapp/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.markrizkalla.dishapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.markrizkalla.dishapp.databinding.ActivitySplachBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -10,5 +13,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplachBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        //        HDE STATUS BAR
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+
     }
 }
