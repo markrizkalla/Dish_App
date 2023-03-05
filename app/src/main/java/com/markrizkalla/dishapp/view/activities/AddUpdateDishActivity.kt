4a71461d518d305f977eb1ad818256fc6990eea9
mark <mark.rizkalla1@gmail.com>
2023-03-05 03:12:22 +0200

    customDialog

--- app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -1,12 +1,43 @@
 package com.markrizkalla.dishapp.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.markrizkalla.dishapp.R
+import com.markrizkalla.dishapp.databinding.ActivityAddUpdateDishBinding
+import com.markrizkalla.dishapp.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddUpdateDishBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivAddDishImage.setOnClickListener {
+            customImageSelectionDialog()
+        }
+    }
+
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+       binding.tvCamera.setOnClickListener {
+           Toast.makeText(this,"Camera clicked",Toast.LENGTH_LONG).show()
+           dialog.dismiss()
+       }
+        binding.tvGallery.setOnClickListener {
+           Toast.makeText(this,"Gallery clicked",Toast.LENGTH_LONG).show()
+            dialog.dismiss()
+       }
+
+
+        dialog.show()
+
+
     }
 }
