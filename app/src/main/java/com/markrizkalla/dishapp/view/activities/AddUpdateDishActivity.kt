15938266b1a192d4ae01485585f50b3e326ec3ad
mark <mark.rizkalla1@gmail.com>
2023-03-06 02:37:31 +0200

    requestingPermissions

--- app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -10,10 +10,18 @@ import com.markrizkalla.dishapp.databinding.ActivityAddUpdateDishBinding
 import com.markrizkalla.dishapp.databinding.DialogCustomImageSelectionBinding
 import android.Manifest
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateDishActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddUpdateDishBinding
@@ -37,7 +45,6 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
            Dexter.withContext(this).withPermissions(
                Manifest.permission.READ_EXTERNAL_STORAGE,
-               Manifest.permission.WRITE_EXTERNAL_STORAGE,
                Manifest.permission.CAMERA
            ).withListener(object :MultiplePermissionsListener{
                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
@@ -50,17 +57,56 @@ class AddUpdateDishActivity : AppCompatActivity() {
                    p1: PermissionToken?
                ) {
                    // show alert dialog
+                   showDialogForPermissions()
                }
            }
            ).onSameThread().check()
            dialog.dismiss()
        }
         binding.tvGallery.setOnClickListener {
-           Toast.makeText(this,"Gallery clicked",Toast.LENGTH_LONG).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ).withListener(object : PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+
+
+            }).onSameThread().check()
             dialog.dismiss()
-       }
+        }
 
         dialog.show()
 
     }
+    private fun showDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("Permissions Required")
+            .setPositiveButton("Go To SETTINGS")
+            {_,_ -> try{
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package",packageName,null)
+                intent.data = uri
+                startActivity(intent)
+            }catch (e: ActivityNotFoundException){
+                e.printStackTrace()
+            }
+
+            }.setNegativeButton("Cancel"){
+                dialog,_ -> dialog.dismiss()
+            }.show()
+
+    }
+
+
 }
