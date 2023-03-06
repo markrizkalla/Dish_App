166131026e9184c2bba833c5a1f3886cd0bf04cf
mark <mark.rizkalla1@gmail.com>
2023-03-06 02:10:14 +0200

    addingPermissions

--- app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/markrizkalla/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -4,9 +4,16 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.karumi.dexter.Dexter
 import com.markrizkalla.dishapp.R
 import com.markrizkalla.dishapp.databinding.ActivityAddUpdateDishBinding
 import com.markrizkalla.dishapp.databinding.DialogCustomImageSelectionBinding
+import android.Manifest
+import android.app.AlertDialog
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddUpdateDishBinding
@@ -27,7 +34,25 @@ class AddUpdateDishActivity : AppCompatActivity() {
         dialog.setContentView(binding.root)
 
        binding.tvCamera.setOnClickListener {
-           Toast.makeText(this,"Camera clicked",Toast.LENGTH_LONG).show()
+
+           Dexter.withContext(this).withPermissions(
+               Manifest.permission.READ_EXTERNAL_STORAGE,
+               Manifest.permission.WRITE_EXTERNAL_STORAGE,
+               Manifest.permission.CAMERA
+           ).withListener(object :MultiplePermissionsListener{
+               override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                   if (p0!!.areAllPermissionsGranted()){
+                       Toast.makeText(this@AddUpdateDishActivity,"You have camera permission",Toast.LENGTH_SHORT).show()
+                   }
+               }
+               override fun onPermissionRationaleShouldBeShown(
+                   p0: MutableList<PermissionRequest>?,
+                   p1: PermissionToken?
+               ) {
+                   // show alert dialog
+               }
+           }
+           ).onSameThread().check()
            dialog.dismiss()
        }
         binding.tvGallery.setOnClickListener {
@@ -35,9 +60,7 @@ class AddUpdateDishActivity : AppCompatActivity() {
             dialog.dismiss()
        }
 
-
         dialog.show()
-
 
     }
 }
