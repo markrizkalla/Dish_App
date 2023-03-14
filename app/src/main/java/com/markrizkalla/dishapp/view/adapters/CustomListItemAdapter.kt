b2c8157dcedc98d3be016d20f6137fce79fef932
Mark Rizkalla <46606022+markrizkalla@users.noreply.github.com>
2023-03-14 23:49:52 +0200

    Update CustomListItemAdapter.kt

--- app/src/main/java/com/markrizkalla/dishapp/view/adapters/CustomListItemAdapter.kt
+++ app/src/main/java/com/markrizkalla/dishapp/view/adapters/CustomListItemAdapter.kt
@@ -11,7 +11,6 @@ class CustomListItemAdapter(
     private val listItems:List<String>) : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>() {
 
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding:ItemCustomListBinding = ItemCustomListBinding.inflate(LayoutInflater.from(activity),parent,false)
         return ViewHolder(binding)
