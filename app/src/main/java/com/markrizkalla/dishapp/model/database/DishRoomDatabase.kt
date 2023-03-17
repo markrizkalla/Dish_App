5400f2a0be340d51aea400617d48235367b69e3f
mark <mark.rizkalla1@gmail.com>
2023-03-17 21:01:03 +0200

    dishRoomDatabase

---
+++ app/src/main/java/com/markrizkalla/dishapp/model/database/DishRoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.markrizkalla.dishapp.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.markrizkalla.dishapp.model.entities.Dish
+
+@Database(entities = [Dish::class], version = 1, exportSchema = false)
+abstract class DishRoomDatabase:RoomDatabase() {
+    abstract val dishDao: DishDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE:DishRoomDatabase? = null
+
+        fun getInstance(context: Context):DishRoomDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        DishRoomDatabase::class.java,
+                        "dish_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
+}
