fc6ea08406836a6fc0f96d8b226e3d049da8c204
mark <mark.rizkalla1@gmail.com>
2023-04-20 15:09:18 +0200

    backgroundToDominateColorPalette

--- app/src/main/java/com/markrizkalla/dishapp/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/markrizkalla/dishapp/view/fragments/DishDetailsFragment.kt
@@ -1,13 +1,20 @@
 package com.markrizkalla.dishapp.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.markrizkalla.dishapp.databinding.FragmentDishDetailsBinding
 import java.io.IOException
 
@@ -33,12 +40,43 @@ class DishDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args:DishDetailsFragmentArgs by navArgs()
-        Log.i("Dish Title",args.dishDetails.title)
+
 
         args.let {
 
             try {
-                Glide.with(requireActivity()).load(it.dishDetails.image).into(binding.ivDishImage)
+                Glide.with(requireActivity()).load(it.dishDetails.image).centerCrop()
+                    .listener(object :RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource?.toBitmap()?.let { it1 ->
+                                Palette.from(it1).generate(){
+
+                                        palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb?:0
+                                    binding.rlDishDetailMain.setBackgroundColor(intColor)
+                                }
+                            }
+
+                            return false
+                        }
+
+                    })
+                    .into(binding.ivDishImage)
             }catch (e:IOException){
                 e.printStackTrace()
             }
